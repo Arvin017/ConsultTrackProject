e309cdd4d7be82bebccd1e982b44f29f409f7dc2
Arvin <arvin@example.com>
2026-06-13 14:06:06 +0530

    Fix API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+// const api = axios.create({ baseURL: '/api' });
+
+const api = axios.create({
+  baseURL: 'https://consulttrackproject.onrender.com/api'
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
